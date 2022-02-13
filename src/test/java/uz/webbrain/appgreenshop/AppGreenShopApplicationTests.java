7d8878e73b7422e7b85aa80067dd2950f59e97c2
KhamzaKuranbayev <khamza@inbox.ru>
2022-02-13 15:41:19 +0500

    Initial commit

---
+++ src/test/java/uz/webbrain/appgreenshop/AppGreenShopApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.webbrain.appgreenshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGreenShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
